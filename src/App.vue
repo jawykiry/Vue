a1db1e3f78e25b256cefdefd9ae11a30e20bdc0a
jawykiry <jawykiry@qq.com>
2020-08-01 21:08:29 +0800

    第一次上传，初始化vue-router

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>导航栏 |
+    <router-link to="/">Frank</router-link> |
+    <router-link to="/xxx">Frank2</router-link>
+  </div>
+  <hr>
+  <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
---
+++ src/components/Frank.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>hi</div>
+</template>
+
+<script>
+    export default {
+        name: "Frank"
+    }
+</script>
+
+<style scoped>
+
+</style>
